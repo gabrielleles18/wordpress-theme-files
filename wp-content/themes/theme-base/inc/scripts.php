2e6670b298d70b6bced08385458122331d06e3ff
gabrielleles18 <gabriel.leles18@gmail.com>
2023-06-03 21:49:38 -0300

    Update

--- wp-content/themes/theme-base/inc/scripts.php
+++ wp-content/themes/theme-base/inc/scripts.php
@@ -3,7 +3,7 @@ add_action( 'wp_enqueue_scripts', function () {
     $theme_version = get_bloginfo( "version" );
     
     wp_enqueue_style(
-        get_stylesheet() . "styles",
+        get_stylesheet() . "-styles",
         get_template_directory_uri() . '/bundle/index.css',
         false,
         $theme_version,
@@ -11,7 +11,7 @@ add_action( 'wp_enqueue_scripts', function () {
     );
     
     wp_enqueue_script(
-        get_stylesheet() . "script",
+        get_stylesheet() . "-script",
         get_template_directory_uri() . '/bundle/index.js',
         false,
         $theme_version,
